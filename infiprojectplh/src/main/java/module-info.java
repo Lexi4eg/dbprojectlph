8d18938323ede0204bc3c31d4a7a7fa10be9e867
Lexi4eg <felix.prattes@gmail.com>
2023-05-05 11:05:58 +0200

    Test

---
+++ infiprojectplh/src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module com.example.infiprojectplh {
+    requires javafx.controls;
+    requires javafx.fxml;
+            
+                            
+    opens com.example.infiprojectplh to javafx.fxml;
+    exports com.example.infiprojectplh;
+}
