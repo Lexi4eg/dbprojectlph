7ff2bee4e27c4f8c05e8d06b84cc9eb31375be83
ju717 <julia.ha2g@outlook.com>
2023-06-15 14:29:01 +0200

    anmeldung3

--- Infi_Project_PLH/src/main/java/model/Anmeldung.java
+++ Infi_Project_PLH/src/main/java/model/Anmeldung.java
@@ -5,14 +5,42 @@ public class Anmeldung{
     private String gender;
 
 
-    public Anmledung (String firstname, String lastname, date Birthdate, String gender){
+    public Anmeldung (String firstname, String lastname, date Birthdate, String gender){
         this.firstname = firstname;
         this.lastname = lastname;
         this.birthdate = birthdate;
         this.gender = gender;
     }
 
-    public int getFi
+    public String getFirstname() {
+        return firstname;
+    }
 
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
 
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public date getBirthdate() {
+        return Birthdate;
+    }
+
+    public void setBirthdate(date birthdate) {
+        Birthdate = birthdate;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
 }
