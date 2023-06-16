ab190435f20eeb6d247b4824081021773455e3fe
ju717 <julia.ha2g@outlook.com>
2023-06-16 10:13:37 +0200

    anmeldung dao update

--- Infi_Project_PLH/src/main/java/model/AnmeldungDAO.java
+++ Infi_Project_PLH/src/main/java/model/AnmeldungDAO.java
@@ -9,7 +9,43 @@ import util.DBConnector;
 import util.DML_DAO;
 
 public class AnmeldungDAO {
-        private int test;
+
+        public static ObservableList<Laufer> getLaefer () {
+                ObservableList<Laufer> lauferObservableList =
+                        FXCollections.observableArrayList();
+                Connection con;
+                Rangliste an1;
+
+                try {
+                        con = DBConnector.connect();
+                        ResultSet rs = con.createStatement().executeQuery(sql);
+                        System.out.println("con: " + con);
+                        String sql = "SELECT * FROM lauefer ";
+
+                        ResultSet rs = con.createStatement().executeQuery(sql);
+
+                        while (rs.next()) {
+                                an1 = new Laufer(
+                                        rs.getInt("Lauefer_ID"),
+                                        rs.getInt("Vorname"),
+                                        rs.getTime("Nachname"),
+                                );
+                                Rangliste.add(an1);
+                        }
+                } catch (SQLException ex) {
+                        System.err.println(ex.getMessage());
+                }
+
+                return Rangliste;
+        }
+
+        public static void saveLaufer (Lauefer lauefer){
+                String sql = "Insert into kunde (Vorname, Nachname) values("
+                        + " " + kunde.getVorname() + " , "
+                        + " " + kunde.getNachname() + " , ";
+                System.out.println("sql: " + sql);
+                DML_DAO.executeDML(sql);
+        }
 
 }
 
